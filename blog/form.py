a4b89276c70995d848283d88e79ada7cc9121426
cho-namok <ican1472@gmail.com>
2020-04-12 00:07:23 +0900

    Added views to create/edit blog post inside the site.

--- blog/form.py
+++ blog/form.py
@@ -1,4 +1,5 @@
 from django import forms
+
 from .models import Post
 
 
@@ -7,4 +8,4 @@ class PostForm(forms.ModelForm):
     # 이 폼을 만들기 위해서 어떤 model이 쓰여야 하는지 장고에 알려주는 구문
     class Meta:
         model = Post
-        fields = ('title', 'text')
+        fields = ('title', 'text',)
--- blog/views.py
+++ blog/views.py
@@ -4,20 +4,6 @@ from .models import Post
 from .form import PostForm
 
 
-def post_new(request):
-    if request.method == 'POST':
-        form = PostForm(request.POST)
-        if form.is_valid():
-            post = form.save(commit=False)
-        post.author = request.user
-        post.published_date = timezone.now()
-        post.save()
-        return redirect('post_detail', pk=post.pk)
-    else:
-        form = PostForm()
-    return render(request, 'blog/post_edit.html', {'form': form})
-
-
 def post_list(request):
     posts = Post.objects.filter(
         published_date__lte=timezone.now()).order_by('published_date')
@@ -29,10 +15,15 @@ def post_detail(request, pk):
     return render(request, 'blog/post_detail.html', {'post': post})
 
 
+def post_new(request):
+    form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
-        form = PostForm(request.POST, instance=post)
+        form = PostForm(instance=post)
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
