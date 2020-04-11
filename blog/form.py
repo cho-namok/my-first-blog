1667a777e427c079c0f60e2db2c9783ae8cd378b
cho-namok <ican1472@gmail.com>
2020-04-11 23:15:45 +0900

    Added view and template for detailed blog post as well as CSS for the site.

---
+++ blog/form.py
@@ -0,0 +1,10 @@
+from django import forms
+from .models import Post
+
+
+class PostForm(forms.ModelForm):
+
+    # 이 폼을 만들기 위해서 어떤 model이 쓰여야 하는지 장고에 알려주는 구문
+    class Meta:
+        model = Post
+        fields = ('title', 'text')
